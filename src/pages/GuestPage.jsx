79fe9c38e769b1c029a8120d069cd4c3fbf64e78
gushchina-alena <gushchina.alenai@yandex.ru>
2021-12-22 18:30:48 +0300

    реализован редирект на страницу логина

--- src/pages/GuestPage.jsx
+++ src/pages/GuestPage.jsx
@@ -1,7 +1,14 @@
 import tasks from '../assets/images/tasks.jpg';
 import '../assets/styles/GuestPage.css';
+import { useHistory } from "react-router-dom";
 
 const GuestPage= () => {
+    let guestHistory = useHistory();
+
+    function onProceed () {
+        guestHistory.push("/login");
+    }
+
     return (
         <div className='tasks'>
             <div className='tasks-guest'>
@@ -10,7 +17,7 @@ const GuestPage= () => {
                 </div>
                 <div className='tasks-guest__text'>
                     <h1>Let's get started by creating the first task</h1>
-                    <button className='tasks-guest__button button'>Proceed</button>
+                    <button className='tasks-guest__button button' onClick={onProceed}>Proceed</button>
                 </div>
             </div>
         </div>
